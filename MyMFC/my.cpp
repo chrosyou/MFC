019d161ba8c9e03a94ba7bfed900a4deba54ac75
chrosyou <646680299@qq.com>
2014-08-04 18:03:35 +0800

    create 8.04

--- MyMFC/my.cpp
+++ MyMFC/my.cpp
@@ -1,10 +1,33 @@
 
 #include "my.h"
-
+using namespace std;
 CMyWinApp theApp;
 
 int main()
 {
-	CWinApp* pApp = AfxGetApp();
+	CWinApp* pMyDoc = AfxGetApp();
+	
+	
+	cout << "pMyDoc->IsKindOf(RUNTIME_CLASS(CDocument))  " << pMyDoc->IsKindOf(RUNTIME_CLASS(CDocument))    << "\n";
+	cout << "pMyDoc->IsKindOf(RUNTIME_CLASS(CCmdTarget)) " << pMyDoc->IsKindOf(RUNTIME_CLASS(CCmdTarget))   << "\n";
+	cout << "pMyDoc->IsKindOf(RUNTIME_CLASS(CObject))    " << pMyDoc->IsKindOf(RUNTIME_CLASS(CObject))      << "\n";
+	cout << "pMyDoc->IsKindOf(RUNTIME_CLASS(CWinApp))    " << pMyDoc->IsKindOf(RUNTIME_CLASS(CWinApp))      << "\n";
+	cout << "pMyDoc->IsKindOf(RUNTIME_CLASS(CView))      " << pMyDoc->IsKindOf(RUNTIME_CLASS(CView))        << "\n";
 	return 0;
+}
+
+void PrintAllClasses()
+{
+	CRuntimeClass* cPrint = NULL;
+	cPrint = CRuntimeClass::pFirstClass;
+	while (cPrint != NULL)
+	{
+		if (cPrint->m_pBaseClass != NULL)
+		{
+			std::cout<<cPrint->m_lpszClassName<<"	"<<cPrint->m_nObjectSize<<"	"<<cPrint->m_pBaseClass->m_lpszClassName<<std::endl;
+		}
+		else std::cout<<cPrint->m_lpszClassName<<"	"<<cPrint->m_nObjectSize<<"	NULL"<<std::endl;
+		
+		cPrint = cPrint->m_pNextClass;
+	}
 }
